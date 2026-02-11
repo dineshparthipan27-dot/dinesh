865c3f492684ebe893e4b98349f2459d14ac4c70
dineshparthipan27-dot <dineshparthipan27@gmail.com>
2026-02-11 14:55:10 +0530

    initial commit by dinesh

---
+++ script.js
@@ -0,0 +1,96 @@
+//Task1
+var a;
+a = 10;
+a = 20;
+var a = 30;
+console.log(a);
+
+//Task2
+let b;
+b = 10;
+console.log(b);
+//b = 20; not accept
+console.log(b);
+
+//Task3 
+const c =10
+// c = 20 not accept reassignind
+
+//Task4
+var name = "dinesh";
+console.log(name);
+
+let age = 22;
+console.log(age);
+
+const city = "salem";
+console.log(city);//type error
+
+//Task5
+let d =10
+console.log(d);
+console.log(100);
+alert("hello i am dinesh")
+//document writein
+document.writeln(d)
+
+//Task6
+ let e = confirm("Do you want to confirm?");
+
+  // result in console
+  console.log(e);
+
+  //Task7
+  let din = prompt("Dinesh");
+  console.log( din);
+ //document.writeln()
+  document.writeln("Hello" + din);
+
+  //Task8
+let hi = "vijay";
+console.log(hi);//normal
+//  console.warn()
+console.warn(hi);//warning massagr
+// console.error()
+console.error(hi);//erroe massage
+
+//Task9
+var price = 100;
+console.log(price);//Before 100
+price = 200;
+console.log(price);//affer 200
+
+//Task10
+let g = true;
+g = false;
+console.log(g);//Final value: false
+
+//Task11
+let f = "Welcome to movie";
+console.log(f);// Welcome to movie
+f = "Movie is fun!";
+console.log(f);//Movie is fun
+
+//Task12
+var count = 10;
+var count = 20;
+var count = 30;
+console.log(count);//30
+
+//Task13
+const user = {
+  name: "Dinesh",
+  age: 22,
+  city: "Salem"
+};
+user.age = 23;
+console.log(user);//name:Dinesh,age:23,city:Salem
+
+//Task14
+var place = "salem";
+let x = 22;
+const country = "India"
+
+console.log(place);
+console.warn(x);
+console.error(country);
